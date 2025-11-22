8552807675357274578febc3c8fe37c8e6a430a7
eggplants <w10776e8w@yahoo.co.jp>
2025-11-22 13:16:45 +0900

    refactor: pnpm format

--- app/modules/auth.google.server.ts
+++ app/modules/auth.google.server.ts
@@ -1,36 +1,41 @@
-import { Authenticator } from "remix-auth";
-import { GoogleStrategy, type GoogleProfile } from "remix-auth-google";
-import { sessionStorage } from "./session.server";
-import { findUserByEmail, createGoogleUser } from "./db.server";
-import { z } from "zod";
+import { Authenticator } from 'remix-auth';
+import { GoogleStrategy, type GoogleProfile } from 'remix-auth-google';
+import { sessionStorage } from './session.server';
+import { findUserByEmail, createGoogleUser } from './db.server';
+import { z } from 'zod';
 /*
 ブラウザ側に露出しうるユーザーのデータのスキーマ
 */
 export const exposedUserSchema = z.object({
   userUuid: z.string(),
   email: z.string(),
-  userAuthType: z.enum(["Email", "Google"]),
-  photoUrl: z.string().optional()
+  userAuthType: z.enum(['Email', 'Google']),
+  photoUrl: z.string().optional(),
 });
 
 export type ExposedUser = z.infer<typeof exposedUserSchema>;
 
-if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET || !process.env.CLIENT_URL) {
-  throw new Error("Missing environment variables");
+if (
+  !process.env.GOOGLE_CLIENT_ID ||
+  !process.env.GOOGLE_CLIENT_SECRET ||
+  !process.env.CLIENT_URL
+) {
+  throw new Error('Missing environment variables');
 }
 
 const SESSION_SECRET = process.env.HPE_SESSION_SECRET;
 if (!SESSION_SECRET) {
-  throw new Error("Missing SESSION_SECRET environment variable");
+  throw new Error('Missing SESSION_SECRET environment variable');
 }
 
 export const authenticator = new Authenticator<ExposedUser>(sessionStorage);
 
-const googleStrategy = new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.CLIENT_URL}/auth/google/callback`,
-},
+const googleStrategy = new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `${process.env.CLIENT_URL}/auth/google/callback`,
+  },
   async ({ profile }: { profile: GoogleProfile }) => {
     const email = getUserEmail(profile);
     const isUserExists = await judgeIsUserExists(email);
@@ -44,7 +49,7 @@ const googleStrategy = new GoogleStrategy({
     }
     const user = await getUser(email);
     if (!user) {
-      throw new Error("User not found");
+      throw new Error('User not found');
     }
     return {
       userUuid: user.userUuid,
@@ -55,11 +60,10 @@ const googleStrategy = new GoogleStrategy({
   },
 );
 
-
-authenticator.use(googleStrategy, "google");
+authenticator.use(googleStrategy, 'google');
 
 function getUserEmail(profile: GoogleProfile) {
-  return profile.emails?.[0]?.value
+  return profile.emails?.[0]?.value;
 }
 
 async function judgeIsUserExists(email: string) {
